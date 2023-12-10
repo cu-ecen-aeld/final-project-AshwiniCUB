2e81c394c63ef01bf14a4991e153fb9fc2c4c11f
AshwiniCUB <ashwini.patil@colorado.edu>
2023-12-09 20:25:38 -0700

    I2C updated

---
+++ base_external/package/BME280/BME280.mk
@@ -0,0 +1,23 @@
+
+##############################################################
+#
+# BME280
+#
+##############################################################
+
+BME280_VERSION = ''
+BME280_SITE = 'git@github.com:AshwiniCUB/final-project-aesd.git'
+BME280_SITE_METHOD = git
+BME280_GIT_SUBMODULES = YES
+
+define BME280_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/measure all
+endef
+
+
+define BME280_INSTALL_TARGET_CMDS
+	 $(INSTALL) -m 0755 $(@D)/BME280/bme280 $(TARGET_DIR)/usr/bin
+endef
+
+
+$(eval $(generic-package))
---
+++ base_external/rootfs_overlay/etc/init.d/S98modules.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+
+case "$1" in
+    start)
+        echo "Initialization of BME280 module"
+        modprobe i2c-dev
+        modprobe i2c-bcm2835
+        i2cdetect -y 1
+        echo "Module loaded succesfully"
+
+        cd /usr/bin
+        ./bme280
+        echo "BME280 sensor module started"
+
+        ;;
+    stop)
+        echo "Removing BME280 modules"
+        rmmod i2c-bcm2835
+        rmmo i2c-dev
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+
+exit 0
